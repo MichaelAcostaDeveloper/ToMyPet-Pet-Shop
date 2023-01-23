e78e73ca04430eae35987d00ba996118af740b97
MichaelAcostaDeveloper <acostamichael80desarrollo@gmail.com>
2023-01-22 23:54:57 -0500

    Mejoras en el menu principal-creacion de while, break

--- ToMyPet.py
+++ ToMyPet.py
@@ -1,22 +1,34 @@
 #Definicion del metodo run()
 """Este metodo da la bienvenida a todos los usuarios"""
 def run():
-          print(10*'#'+" Bienvenidos a ToMyPet "+10*'#')
-          opcion_ingreso = int(input('''Selecciona el tipo de usuario:
-          1. Manager.
-          2. Cliente.
-          ->'''))
-          if opcion_ingreso == 1:
-                    user = input('Ingrese su usuario: ')
-                    passw = input('Ingrese su password: ')
-                    print('Bienvenido '+user+'.')
-          else:
-                    print('''Estimado cliente seleccione los que necesita:
-                    1. Creacion de codigo QR.
-                    2. Reactivacion del codigo QR.
-                    3. Localizadores y app.
-                    4. Alimentacion perruna.
-                    ->''')
+          while True:
+                    print(10*'#'+" Bienvenidos a ToMyPet "+10*'#')
+                    opcion_ingreso = int(input('''Selecciona el tipo de usuario:
+                    1. Manager.
+                    2. Cliente.
+                    3. Salir.
+                    ->'''))
+                    if opcion_ingreso == 1:
+                              user = input('Ingrese su usuario: ')
+                              passw = input('Ingrese su password: ')
+                              print('Bienvenido '+user+'.')
+                              #CODIGO PARA INGRESAR AL SITIO COMO MANAGER
+                              break #hace que el ciclo termine en este punto
+                    elif opcion_ingreso == 2:
+                              print('''Estimado cliente seleccione la opcion que necesita:
+                              1. Creacion de codigo QR.
+                              2. Reactivacion del codigo QR.
+                              3. Localizadores y app.
+                              4. Alimentacion perruna.
+                              ->''')
+                              #CODIGO DE EVALUACION DE LAS OPCIONES
+                              break #hace que el ciclo  termine en este punto
+                    elif opcion_ingreso == 3:
+                              print('Un placer servirle!')
+                              return False #cierra el ciclo while
+                    else:
+                              print('Ingrese una opcion valida!!!')
+
 
 
 
